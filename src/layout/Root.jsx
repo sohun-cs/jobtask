7f81d72b373abad1e11346c137986a9e2d92c9e0
sohun-cs <asaduzzamansohun5@gmail.com>
2024-08-17 11:11:46 +0600

    page height and width fixed

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -9,7 +9,9 @@ const Root = () => {
     return (
         <div className='container mx-auto'>
             <Navbar></Navbar>
-            <Outlet></Outlet>
+            <div className='min-h-[calc(100vh-68px-68px)]'>
+                <Outlet></Outlet>
+            </div>
             <Footer></Footer>
         </div>
     );
